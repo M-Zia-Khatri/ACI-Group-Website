e5cbd0d1c20a314e426db06d45c1a869d55b30b8
SoroCars <soro.finance.pk@gmail.com>
2025-06-15 17:18:01 +0500

    fix font problem in problem section.

--- src/components/home/ProblemSection.tsx
+++ src/components/home/ProblemSection.tsx
@@ -2,14 +2,14 @@ export default function ProblemSection() {
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 bg-[var(--background)] text-[var(--text-black)]">
       <div className="flex flex-col justify-center text-center md:text-left px-4 py-6 text-white">
-        <h2 className="text-3xl md:text-3xl font-bold mb-4 text-[var(--primary-color)] tracking-wider">
+        <h2 className="text-fluid-lg font-bold mb-4 text-[var(--primary-color)] tracking-widest">
           Feeling overwhelmed by trading, losses, or unreliable “tips”?
         </h2>
         <p className="mb-4 text-base md:text-lg ">
           You’re not alone. Most people don’t have the time or experience to
           trade smartly. The wrong move can wipe out months of savings.
         </p>
-        <p className="text-base md:text-lg ">
+        <p className="text-base md:text-xl ">
           What if someone skilled could trade for you — with your money, your
           control, and your peace of mind?
         </p>
