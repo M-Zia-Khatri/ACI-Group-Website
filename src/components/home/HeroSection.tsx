cbf1c6daf40f69f8d1072368c2f9739325285b50
SoroCars <soro.finance.pk@gmail.com>
2025-06-16 23:49:06 +0500

    fix font in this file.

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -6,13 +6,13 @@ export default function HeroSection() {
   return (
     <section className="w-full h-[90vh] bg-[var(--dark-navy)] text-white flex items-center justify-center relative overflow-hidden">
       <motion.div
-        className="mx-4 py-24 text-center z-10 relative tracking-wider"
+        className="mx-4 py-24 text-center z-10 relative "
         initial={{ opacity: 0, y: 40 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, ease: "easeOut" }}
       >
         <motion.h1
-          className="text-fluid-2xl font-[var(--Bebas-Neue)] mb-6 "
+          className="text-fluid-2xl sm:text-fluid-3xl md:text-fluid-4xl lg:text-fluid-5xl Karla mb-6 font-bold tracking-tight"
           initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.2, duration: 0.7 }}
@@ -20,12 +20,12 @@ export default function HeroSection() {
           Turn Financial Confusion into Confidence
         </motion.h1>
         <motion.p
-          className="max-w-2xl mx-auto text-fluid font-roboto mb-8 tracking-wider"
+          className="max-w-3xl mx-auto text-fluid md:text-fluid-lg lg:text-fluid-xl font-Newsreader mb-8 tracking-wider"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.4, duration: 0.7 }}
         >
-          Expert-led copy trading in Forex, Crypto & Stocks to help you grow
+          Expert-led copy trading in Fores, Crypto & Stocks to help you grow
           your wealth without stress.
         </motion.p>
         <motion.a
@@ -43,7 +43,7 @@ export default function HeroSection() {
           >
             <Button
               variant="ghost"
-              className="cta text-fluid-lg hover:bg-[#1C1D1D] btn transition-colors duration-200 hover:text-[var(--primary-blue)]"
+              className="cta text-fluid md:text-fluid-lg lg:text-fluid-xl hover:bg-[#1C1D1D] btn transition-colors duration-200 hover:text-[var(--primary-blue)] font-semibold"
             >
               Book a Free Strategy Call
             </Button>
